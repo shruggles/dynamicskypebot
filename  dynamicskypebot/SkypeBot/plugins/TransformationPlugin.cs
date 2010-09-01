c2800370636c7cfa2fbead0652e2c53ba1b89c6b
Eckankar <Eckankar@d44c2b64-5b73-11de-8abc-d1c337b90d21>
2010-09-01 16:33:30 +0000

    Transformation: +Z̈̇̿̚a͖̟̝͔ͥ̓͌͛̀ͧͅl͌g͔̦͎̦̝͇̲̒ͭ̉͒o̭̪̫̰̪̗̅̅̊

---  dynamicskypebot/SkypeBot/plugins/TransformationPlugin.cs
+++  dynamicskypebot/SkypeBot/plugins/TransformationPlugin.cs
@@ -38,6 +38,7 @@ namespace SkypeBot.plugins {
                         new Transformation("Reverse", ReverseTransform),
                         new Transformation("MiXeD CaSe", MixedCaseTransform),
                         new Transformation("pǝddılɟ", FlipTransform),
+                        new Transformation("Z̈̇̿̚a͖̟̝͔ͥ̓͌͛̀ͧͅl͌g͔̦͎̦̝͇̲̒ͭ̉͒o̭̪̫̰̪̗̅̅̊", ZalgoTransform),
                         new Transformation("Random poop (1/20 chance)", (src) => PoopTransform(src, 20)),
                         new Transformation("Random poop (1/500 chance)", (src) => PoopTransform(src, 500)),
                         new Transformation("Random poop (1/3000 chance)", (src) => PoopTransform(src, 3000)),
@@ -125,6 +126,70 @@ namespace SkypeBot.plugins {
                     output = flipTable[c] + output;
                 } else {
                     output = c + output;
+                }
+            }
+
+            return output;
+        }
+
+        private static string ZalgoTransform(string source) {
+            // Adapted from www.marlborotech.com/Zalgo.html
+
+            List<char> zalgoUp = new List<char>() {
+		        '\u030d', '\u030e', '\u0304', '\u0305', 
+		        '\u033f', '\u0311', '\u0306', '\u0310', 
+		        '\u0352', '\u0357', '\u0351', '\u0307', 
+		        '\u0308', '\u030a', '\u0342', '\u0343', 
+		        '\u0344', '\u034a', '\u034b', '\u034c', 
+		        '\u0303', '\u0302', '\u030c', '\u0350', 
+		        '\u0300', '\u0301', '\u030b', '\u030f', 
+		        '\u0312', '\u0313', '\u0314', '\u033d', 
+		        '\u0309', '\u0363', '\u0364', '\u0365', 
+		        '\u0366', '\u0367', '\u0368', '\u0369', 
+		        '\u036a', '\u036b', '\u036c', '\u036d', 
+		        '\u036e', '\u036f', '\u033e', '\u035b', 
+		        '\u0346', '\u031a' 
+            };
+
+            List<char> zalgoMid = new List<char>() {
+		        '\u0315', '\u031b', '\u0340', '\u0341', 
+		        '\u0358', '\u0321', '\u0322', '\u0327', 
+		        '\u0328', '\u0334', '\u0335', '\u0336', 
+		        '\u034f', '\u035c', '\u035d', '\u035e', 
+		        '\u035f', '\u0360', '\u0362', '\u0338', 
+		        '\u0337', '\u0361', '\u0489' 
+	        };
+
+            List<char> zalgoDown = new List<char>() {
+		        '\u0316', '\u0317', '\u0318', '\u0319', 
+		        '\u031c', '\u031d', '\u031e', '\u031f', 
+		        '\u0320', '\u0324', '\u0325', '\u0326', 
+		        '\u0329', '\u032a', '\u032b', '\u032c', 
+		        '\u032d', '\u032e', '\u032f', '\u0330', 
+		        '\u0331', '\u0332', '\u0333', '\u0339', 
+		        '\u033a', '\u033b', '\u033c', '\u0345', 
+		        '\u0347', '\u0348', '\u0349', '\u034d', 
+		        '\u034e', '\u0353', '\u0354', '\u0355', 
+		        '\u0356', '\u0359', '\u035a', '\u0323' 
+            };
+
+            String output = "";
+
+            foreach (char c in source) {
+                output += c;
+
+                int numUp = random.Next(16) / 2 + 1;
+                int numMid = random.Next(6) / 2;
+                int numDown = random.Next(16) / 2 + 1;
+
+                for (int i = 0; i < numUp; i++) {
+                    output += zalgoUp[random.Next(zalgoUp.Count)];
+                }
+                for (int i = 0; i < numMid; i++) {
+                    output += zalgoMid[random.Next(zalgoMid.Count)];
+                }
+                for (int i = 0; i < numDown; i++) {
+                    output += zalgoDown[random.Next(zalgoDown.Count)];
                 }
             }
 
