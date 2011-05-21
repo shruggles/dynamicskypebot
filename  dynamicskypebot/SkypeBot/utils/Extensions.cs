f4cf7389e58578089e7aa81ae3def2bcd088f765
Eckankar <Eckankar@d44c2b64-5b73-11de-8abc-d1c337b90d21>
2011-05-21 10:34:55 +0000

    Add String.FormatWith

---
+++  dynamicskypebot/SkypeBot/utils/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkypeBot.utils {
+    public static class Extensions {
+        public static String FormatWith(this String s, params object[] args) {
+            return String.Format(s, args);
+        }
+
+        public static String FormatWith(this String s, object arg) {
+            return String.Format(s, arg);
+        }
+    }
+}
