0ed0ed23b4503455b8f43ef774c2cde904f8d1ba
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2013-07-01 22:12:57 +0200

    Fix UrbanDictionary

--- SkypeBot/plugins/UrbanDictionaryPlugin.cs
+++ SkypeBot/plugins/UrbanDictionaryPlugin.cs
@@ -8,19 +8,13 @@ using System.Net;
 using System.IO;
 using System.Windows.Forms;
 using SKYPE4COMLib;
-using SkypeBot.UrbanDictionary;
+using Procurios.Public;
 using log4net;
 
 namespace SkypeBot.plugins {
     public class UrbanDictionaryPlugin : Plugin {
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         
-        // Signups for keys are closed
-        // Found it at https://cyanox.nl/trac/noxbot/changeset/21
-        private const string KEY = "a237993550175803efbf9530ff4de2bc";
-
-        private UrbanSearchPortTypeClient dict;
-
         public String name() { return "UrbanDictionary plugin"; }
 
         public String help() { return "!urban <word>"; }
@@ -31,7 +25,6 @@ namespace SkypeBot.plugins {
         public void openConfig() { }
 
         public UrbanDictionaryPlugin() {
-            dict = new UrbanSearchPortTypeClient();
         }
 
         public void load() {
@@ -45,25 +38,33 @@ namespace SkypeBot.plugins {
         public void Skype_MessageStatus(IChatMessage message, TChatMessageStatus status) {
             Match output = Regex.Match(message.Body, @"^!urban (.*)", RegexOptions.IgnoreCase);
             if (output.Success) {
-                String queryString = output.Groups[1].Value;
-                if (!dict.verify_key(KEY)) {
-                    log.Error("Invalid key used for dictionary lookup.");
-                    message.Chat.SendMessage("The automated Urban Dictionary lookup is down until further notice.");
+                String query = output.Groups[1].Value;
+                
+                WebRequest req = (HttpWebRequest) HttpWebRequest.Create("http://api.urbandictionary.com/v0/define?page=1&term=" + System.Uri.EscapeDataString(query));
+                WebResponse resp;
+                try {
+                    resp = req.GetResponse();
+                } catch (WebException) {
+                    log.Warn("UrbanDictionary appears to be unavailable at the moment.");
+                    message.Chat.SendMessage("Sorry, I seem to be unable to contact UrbanDictionary.");
+                    return;
                 }
-                else {
-                    log.Debug("Key verified.");
-                    Definition[] defs = dict.lookup(KEY, queryString);
-                    if (defs.Length <= 0)
-                        message.Chat.SendMessage(String.Format(@"UrbanDictionary lookup of ""{0}"": No results found.", queryString));
-                    else {
-                        message.Chat.SendMessage(String.Format(
-                            "UrbanDictionary lookup of \"{0}\":\n\n{1}: {2}\n\n{3}",
-                            queryString,
-                            defs[0].word,
-                            defs[0].definition,
-                            defs[0].example
-                        ));
-                    }
+
+                String responseText = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+                var defs = (Hashtable) JSON.JsonDecode(responseText);
+
+                if ((string)defs["response_type"] == "no_results") {
+                    message.Chat.SendMessage(String.Format(@"UrbanDictionary lookup of ""{0}"": No results found.", query));
+                } else {
+                    var def = (defs["list"] as ArrayList)[0] as Hashtable;
+
+                    message.Chat.SendMessage(String.Format(
+                        "UrbanDictionary lookup of \"{0}\":\n\n{1}: {2}\n\n{3}",
+                        query,
+                        def["word"],
+                        def["definition"],
+                        def["example"]
+                    ));
                 }
             }
         }
